809f5bafb94375778e93c2f22a5816f47c7f1001
jojo1pf <94373365+Irejoseph@users.noreply.github.com>
2024-03-19 16:49:35 +0000

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Condcomp from "./components/Condcomp";
+import Fruits from "./components/Fruits";
+import Hi from "./components/Hi"
+
+
+function App() {
+  return (
+  <div className="App">
+    <Condcomp />
+  </div>
+  ) 
+}
+
+export default App;
+
